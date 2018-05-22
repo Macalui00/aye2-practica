c05ec4233da66f3eff6dcb8e0ee62a868aaffef7
macalui00 <macarenalui@gmail.com>
2018-05-22 16:24:39 -0300

    modificaciones

--- aye2/src/aye2/practica6/Fecha.java
+++ aye2/src/aye2/practica6/Fecha.java
@@ -12,15 +12,29 @@ public class Fecha {
 		revisarFecha();
 	}
 	 private void revisarFecha() {
-		 if (mes = 2) {
-			 if (esBisiesto(año)) && () {
-				 
+		while (dia > CantDias(mes)){
+			if (dia > CantDias(mes, año)) {
+				dia = dia - CantDias(mes, año);
+				mes = mes + 1;
+				if (mes > 12) {
+					mes = 1;
+					año = año + 1;
+				}
+			} 
+		}
+	 }
+	 private int CantDias(int m, int a) {
+		 if (m=2) {
+			 if esBisiesto(a) {
+				 return 29;
+			 } else {
+				 return 28;
 			 }
 		 }
 	 }
-	 public boolean esBisiesto(int a) {
-		 if (a / 100 != 0) && (a / 4 = 0) && (a/400) = 0) {
-			 return true;
+	 private boolean esBisiesto(int a) {
+		 if ((a/100 != 0) && (a/4 == 0) &&(a/400 == 0)) {
+				 return true;
 		 } else {
 			 return false;
 		 }
