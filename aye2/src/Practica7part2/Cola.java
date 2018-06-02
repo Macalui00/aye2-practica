a4e0d8ad04ca4845eca7e8ed708f14457f1db6c6
macalui00 <macarenalui@gmail.com>
2018-06-02 17:20:23 -0300

    modificaciones

---
+++ aye2/src/Practica7part2/Cola.java
@@ -0,0 +1,59 @@
+package Practica7part2;
+
+public class Cola<T> extends Lista<T>{
+	
+	public Cola() {
+		super();
+	}
+	
+	public Cola(T dato) {
+		super(dato);
+	}
+
+	@Override
+	public int compareTo(Lista<T> o) {
+		return head.compareTo(o.getHead);
+	}
+	
+	@Override
+	public void addElem(T dato) {
+		if (esVacia()) {
+			head = dato;
+		} else {
+			if (tail == null) {
+				tail = new Cola<T>(dato);
+			} else {
+				tail.addElem(dato);
+			}
+		}
+	}
+
+	@Override
+	public boolean esVacia() {
+		if (head == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public T sacarElem() {
+		if (tail.esVacia()) {
+			return head;
+		} else {
+			tail.sacarElem();
+		}
+	}
+	
+	
+	/*if (head == null) {
+		head = dato;
+	} else {
+		if (tail == null) {
+			tail = new Lista<T>(dato);
+		} else {
+			tail.agregarElemento(dato);
+		}
+	}*/
+}
---
+++ aye2/src/Practica7part2/Lista.java
@@ -0,0 +1,34 @@
+package Practica7part2;
+
+public abstract class Lista<T extends Comparable<T>> implements Comparable<Lista<T>> {
+	protected T head;
+	protected Lista<T> tail;
+	
+	public Lista() {
+	}
+	
+	public Lista(T dato) {
+		head = dato;
+	}
+
+	public T getHead() {
+		return head;
+	}
+
+	public void setHead(T head) {
+		this.head = head;
+	}
+
+	public Lista<T> getTail() {
+		return tail;
+	}
+
+	public void setTail(Lista<T> tail) {
+		this.tail = tail;
+	}
+	
+	
+	public abstract void addElem(T dato);
+	public abstract boolean esVacia(); 
+	public abstract T sacarElem();
+}
